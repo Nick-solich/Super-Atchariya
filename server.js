b8e7e56ab3fd82afe24bd6c5643cf291e77bcfd0
Nick <nick.rattanaton@gmail.com>
2023-03-28 23:05:05 +0700

    🐱‍👓 Import Old File

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Load env vars
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+//Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
+//Route files
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(cors());
+//Add cookie parser
+app.use(cookieParser());
+
+//Body parser
+app.use(express.json());
+
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/appointments", appointments);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
